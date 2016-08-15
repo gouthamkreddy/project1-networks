afd22f037a9a732a34177e154ce527f4e4fcf115
gouthamkreddy <greddy@cse.iitk.ac.in>
2016-08-15 18:03:17 +0530

    trying to parse request

--- server.c
+++ server.c
@@ -12,45 +12,64 @@
 #define MAX_SIZE    4096						//Buffer size
 #define PORT    	3000					
 
-int parse(int size, char str[size]){
-	Request = Request-Line
-             *(( general-header
-              | request-header ) CRLF)
-             CRLF
-             [ message-body ]
-    Request-Line = Method SP Request-URI SP HTTP-Version CRLF
-    Method = "GET"
-    HTTP-Version = "HTTP/1.1"
-    Request-URI = "*" | absoluteURI | abs_path | authority
-    // (Files are indicated by an absolute path (abs_path), 
-    // such as “/index.html” (a specific file relative to the
-    // server’s base directory) or such as “/images/" or “/” 
-    // (directories relative to the server's base directory).)
-    Connection = "Connection" ":" 1#(connection-token) connection-token = token
-    request-header = Accept                   
-                      | Accept-Charset           
-                      | Accept-Encoding          
-                      | Accept-Language          
-                      | Authorization            
-                      | Expect               
-                      | From                 
-                      | Host                 
-                      | If-Match             
-                      | If-Modified-Since    
-                      | If-None-Match        
-                      | If-Range             
-                      | If-Unmodified-Since  
-                      | Max-Forwards         
-                      | Proxy-Authorization  
-                      | Range                
-                      | Referer              
-                      | TE                   
-                      | User-Agent           
-    CR             = <US-ASCII CR, carriage return (13)>
-    LF             = <US-ASCII LF, linefeed (10)>
-    SP             = <US-ASCII SP, space (32)>
-    CRLF		   = CR LF
+// int parse(int size, char str[size]){
+// 	Request = Request-Line
+//              *(( general-header
+//               | request-header ) CRLF)
+//              CRLF
+//              [ message-body ]
+//     Request-Line = Method SP Request-URI SP HTTP-Version CRLF
+//     Method = "GET"
+//     HTTP-Version = "HTTP/1.1"
+//     Request-URI = "*" | absoluteURI | abs_path | authority
+//     // (Files are indicated by an absolute path (abs_path), 
+//     // such as “/index.html” (a specific file relative to the
+//     // server’s base directory) or such as “/images/" or “/” 
+//     // (directories relative to the server's base directory).)
+//     Connection = "Connection" ":" 1#(connection-token) connection-token = token
+//     request-header = Accept                   
+//                       | Accept-Charset           
+//                       | Accept-Encoding          
+//                       | Accept-Language          
+//                       | Authorization            
+//                       | Expect               
+//                       | From                 
+//                       | Host                 
+//                       | If-Match             
+//                       | If-Modified-Since    
+//                       | If-None-Match        
+//                       | If-Range             
+//                       | If-Unmodified-Since  
+//                       | Max-Forwards         
+//                       | Proxy-Authorization  
+//                       | Range                
+//                       | Referer              
+//                       | TE                   
+//                       | User-Agent           
+//     CR             = <US-ASCII CR, carriage return (13)>
+//     LF             = <US-ASCII LF, linefeed (10)>
+//     SP             = <US-ASCII SP, space (32)>
+//     CRLF		   = CR LF
 
+// }
+
+// {
+// 	response
+// 	HTTP/1.1 200 OK
+// 	200 OK
+// 	400 Bad Request
+//  	404 Not Found
+// 	500 Internal Server Error
+// 	501 Not Implemented
+// }
+
+char* filename(int size, char str[size]){
+	char *firstline;
+   	firstline = strchr(str, '\n');
+   	int len = firstline - str + 1;
+   	printf("\n\n%.*s\n\n", len, str);
+   	printf("%d\n", len);
+   	return str;
 }
 
 int main(int argc, char * argv[]) {
@@ -115,6 +134,9 @@ int main(int argc, char * argv[]) {
 		
 		// parse(strlen(recv_buf), recv_buf);
 		// printf("size:%d\n", strlen(recv_buf));
+		//Check if string is valid or not
+
+		filename(strlen(recv_buf), recv_buf);
 
 		strcpy(send_buf,"HTTP/1.1 200 OK\nContent-length: 47\nContent-Type: text/html\n\n<html><body><H1>Hello buddy</H1></body></html>");    
 		status = send(new_sockid,&send_buf,sizeof(send_buf),0);
