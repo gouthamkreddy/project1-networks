8a709a0faae10210c68e531a171cbdca4466099b
gouthamkreddy <greddy@cse.iitk.ac.in>
2016-08-16 14:41:14 +0530

    added to check file thing

--- server.c
+++ server.c
@@ -12,58 +12,6 @@
 #define MAX_SIZE    4096						//Buffer size
 #define PORT    	3001
 
-
-// int parse(int size, char str[size]){
-// 	Request = Request-Line
-//              *(( general-header
-//               | request-header ) CRLF)
-//              CRLF
-//              [ message-body ]
-//     Request-Line = Method SP Request-URI SP HTTP-Version CRLF
-//     Method = "GET"
-//     HTTP-Version = "HTTP/1.1"
-//     Request-URI = "*" | absoluteURI | abs_path | authority
-//     // (Files are indicated by an absolute path (abs_path), 
-//     // such as “/index.html” (a specific file relative to the
-//     // server’s base directory) or such as “/images/" or “/” 
-//     // (directories relative to the server's base directory).)
-//     Connection = "Connection" ":" 1#(connection-token) connection-token = token
-//     request-header = Accept                   
-//                       | Accept-Charset           
-//                       | Accept-Encoding          
-//                       | Accept-Language          
-//                       | Authorization            
-//                       | Expect               
-//                       | From                 
-//                       | Host                 
-//                       | If-Match             
-//                       | If-Modified-Since    
-//                       | If-None-Match        
-//                       | If-Range             
-//                       | If-Unmodified-Since  
-//                       | Max-Forwards         
-//                       | Proxy-Authorization  
-//                       | Range                
-//                       | Referer              
-//                       | TE                   
-//                       | User-Agent           
-//     CR             = <US-ASCII CR, carriage return (13)>
-//     LF             = <US-ASCII LF, linefeed (10)>
-//     SP             = <US-ASCII SP, space (32)>
-//     CRLF		   = CR LF
-
-// }
-
-// {
-// 	response
-// 	HTTP/1.1 200 OK
-// 	200 OK
-// 	400 Bad Request
-//  	404 Not Found
-// 	500 Internal Server Error
-// 	501 Not Implemented
-// }
-
 char* parse_req(int sockid, int size, char str[size]){
    	
    	/*--- Method Check ---*/
@@ -95,13 +43,9 @@ int write_response(int sockid, int i){
 	{
 		strcpy(buffer, "HTTP/1.1 501 Not Implemented\r\n");
   		write(sockid, buffer, strlen(buffer));
-  		strcpy(buffer, "Server: Dynamic Thread\n");
-  		write(sockid, buffer, strlen(buffer));
   		strcpy(buffer, "Content-length: 105\r\n");
   		write(sockid, buffer, strlen(buffer));
-  		strcpy(buffer, "Content-Type: text/html\r\n");
-  		write(sockid, buffer, strlen(buffer));
-  		strcpy(buffer, "\r\n");
+  		strcpy(buffer, "Content-Type: text/html\r\n\r\n");
   		write(sockid, buffer, strlen(buffer));
   		strcpy(buffer, "<html>\n<head>\n<title>Not Implemented</title>\n</head>\r\n");
   		write(sockid, buffer, strlen(buffer));
@@ -112,22 +56,40 @@ int write_response(int sockid, int i){
 	{
 		strcpy(send_buf, "HTTP/1.1 400 Bad Request\r\n");
   		write(new_sockid, send_buf, strlen(send_buf));
-  		strcpy(send_buf, "Server: Dynamic Thread\n");
-  		write(new_sockid, send_buf, strlen(send_buf));
   		strcpy(send_buf, "Content-length: 97\r\n");
   		write(new_sockid, send_buf, strlen(send_buf));
-  		strcpy(send_buf, "Content-Type: text/html\r\n");
-  		write(new_sockid, send_buf, strlen(send_buf));
-  		strcpy(send_buf, "\r\n");
+  		strcpy(send_buf, "Content-Type: text/html\r\n\r\n");
   		write(new_sockid, send_buf, strlen(send_buf));
   		strcpy(send_buf, "<html>\n<head>\n<title>Bad Request</title>\n</head>\r\n");
   		write(new_sockid, send_buf, strlen(send_buf));
   		strcpy(send_buf, "<body>\n<p>400 Bad Request</p>\n</body>\n</html>\r\n");
   		write(new_sockid, send_buf, strlen(send_buf));
 	}
-	else if (i == 2)
+	else if (i == 404)
 	{
-		/* code */
+		strcpy(send_buf, "HTTP/1.1 404 Not Found\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		strcpy(send_buf, "Content-length: 93\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		strcpy(send_buf, "Content-Type: text/html\r\n\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		strcpy(send_buf, "<html>\n<head>\n<title>Not Found</title>\n</head>\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		strcpy(send_buf, "<body>\n<p>404 Not Found</p>\n</body>\n</html>\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+	}
+	else if (i == 500)
+	{
+		strcpy(send_buf, "HTTP/1.1 500 Internal Server Error\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		strcpy(send_buf, "Content-length: 117\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		strcpy(send_buf, "Content-Type: text/html\r\n\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		strcpy(send_buf, "<html>\n<head>\n<title>Internal Server Error</title>\n</head>\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		strcpy(send_buf, "<body>\n<p>500 Internal Server Error</p>\n</body>\n</html>\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
 	}
 	return 0;
 }
@@ -193,27 +155,33 @@ int main(int argc, char * argv[]) {
 		}
 
 		char* file_path = parse_req(new_sockid, strlen(recv_buf), recv_buf);
-		// printf("%s\n", file_path);
-		printf("strlen: %d", strlen("<html>\n<head>\n<title>Bad Request</title>\n</head>\r\n<body>\n<p>400 Bad Request.</p>\n</body>\n</html>\r\n"));
 
-		// strcpy(send_buf, "HTTP/1.1 400 Bad Request\r\n");
-  // 		write(new_sockid, send_buf, strlen(send_buf));
-  // 		strcpy(send_buf, "Server: Dynamic Thread\n");
-  // 		write(new_sockid, send_buf, strlen(send_buf));
-  // 		strcpy(send_buf, "Content-length: 97\r\n");
-  // 		write(new_sockid, send_buf, strlen(send_buf));
-  // 		strcpy(send_buf, "Content-Type: text/html\r\n");
-  // 		write(new_sockid, send_buf, strlen(send_buf));
-  // 		strcpy(send_buf, "\r\n");
-  // 		write(new_sockid, send_buf, strlen(send_buf));
-  // 		strcpy(send_buf, "<html>\n<head>\n<title>Bad Request</title>\n</head>\r\n");
-  // 		write(new_sockid, send_buf, strlen(send_buf));
-  // 		strcpy(send_buf, "<body>\n<p>400 Bad Request</p>\n</body>\n</html>\r\n");
-  // 		write(new_sockid, send_buf, strlen(send_buf));
+		FILE *file = fopen(file_path+1,"rb");
+		if (!file){
+	    	printf("no such file exists\n");
+	    } 
+		else 
+	    {
+	   		fseek(file, 0L, SEEK_END);
+		    int count = ftell(file);
+		   	fseek(file, 0L, SEEK_SET);
+	    }
+		fclose(file);
 
-		// strcpy(send_buf,"HTTP/1.1 200 OK\nContent-length: 47\nContent-Type: text/html\n\n<html><body><H1>Hello buddy</H1></body></html>");    
-		// status = send(new_sockid,&send_buf, sizeof(send_buf),0);
-		// status = write(new_sockid,&send_buf,sizeof(send_buf));
+		strcpy(send_buf, "HTTP/1.1 200 OK\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		strcpy(send_buf, "Content-length: count\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		strcpy(send_buf, "Content-Type: text/plain\r\n\r\n");
+  		write(new_sockid, send_buf, strlen(send_buf));
+  		
+
+  		// strcpy(send_buf, "<html>\n<head>\n<title>OK</title>\n</head>\r\n");
+  		// write(new_sockid, send_buf, strlen(send_buf));
+  		// strcpy(send_buf, "<body>\n<p>400 Bad Request</p>\n</body>\n</html>\r\n");
+  		// write(new_sockid, send_buf, strlen(send_buf));
+
+
 		
     	close(new_sockid);	
 	}
